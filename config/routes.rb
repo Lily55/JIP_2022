7fa6f4f2958f3aeef4ba6d82743469ca3041000a
Cheshire_fox <scarmasha@gmail.com>
2022-12-19 01:16:35 +0300

    Hell, yeah. It worksgit add .git add .

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'session#login'
   resources :users
   get 'palindromes/input'
   get 'palindromes/view'
+  post 'authorize', to: "session#authorize"
+  get 'logout', to: "session#logout"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
