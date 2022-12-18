7efc2d8c9fa69635855023f0fae589f1fdeab304
Cheshire_fox <scarmasha@gmail.com>
2022-12-19 00:01:29 +0300

    Before model Number when all works with input and view

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -3,5 +3,13 @@ class PalindromesController < ApplicationController
   end
 
   def view
+    @num = params[:num].to_i
+    @result = check_number(@num)
+  end
+
+  private
+
+  def check_number(numb)
+    (1..numb).select { |x| x.to_s == x.to_s.reverse && x.to_s(2).to_s == x.to_s(2).to_s.reverse }
   end
 end
